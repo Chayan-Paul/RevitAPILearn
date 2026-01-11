231e44009b03d989f144e7afefb3b4f677f3f6c0
Chayan-Paul <Chayan-Paul@users.noreply.github.com>
2026-01-11 12:49:38 +0530

    Set up training material

--- ConnectToRevit/ConnectToRevit/ConnectToRevit.cs
+++ ConnectToRevit/ConnectToRevit/ConnectToRevit.cs
@@ -1,4 +1,13 @@
-﻿using Autodesk.Revit.Attributes;
+﻿// -----------------------------------------------------------------------------
+// PaulAi Academy – Revit API Learning Series
+// This code is provided for educational and learning purposes only.
+// You are free to study, modify, and experiment with it.
+// Revit® is a registered trademark of Autodesk, Inc.
+// -----------------------------------------------------------------------------
+
+
+
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
