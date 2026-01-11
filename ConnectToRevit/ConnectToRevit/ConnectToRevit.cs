ca3b222050893bdc5cb786f62231f2be8206af75
Chayan-Paul <Chayan-Paul@users.noreply.github.com>
2026-01-11 12:46:34 +0530

    Add project files.

---
+++ ConnectToRevit/ConnectToRevit/ConnectToRevit.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace ConnectToRevit
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class ConnectToRevit : IExternalCommand
+    { 
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("ConnectToRevit", "Hello from ConnectToRevit!");
+            return Result.Succeeded;
+        }
+    }
+}
